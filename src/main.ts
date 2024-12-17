bc7d78a51808579c593fa83e5524ccb08415e4f7
Franco Nogarin <spydmobile@gmail.com>
2024-12-17 05:56:31 -0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+const message: string = "Welcome to the Site and Store!";
+console.log(message);
+
+document.body.innerHTML += `<p>${message}</p>`;
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/ThebachaFPV/', // Replace with your repo name
+  server: {
+    port: 5173,
+  },
+});
+
+
