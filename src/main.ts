b8c09eeaf614dbbcd333f0bc2b15729e179d1bd2
Franco Nogarin <spydmobile@gmail.com>
2024-12-17 05:59:06 -0700

    update store

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-const message: string = "Welcome to the Site and Store!";
+const message: string = "Welcome to the ThebachaFPV Site and Store!";
 console.log(message);
 
 document.body.innerHTML += `<p>${message}</p>`;
