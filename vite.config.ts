79c235ee13b1c0c47c8645acbd6278e87ff1d43b
Franco Nogarin <spydmobile@gmail.com>
2025-05-06 16:15:01 -0600

    Update Vite configuration base path to serve site at the root of GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  base: '/ThebachaFPVWeb', // Ensure this matches your GitHub Pages repository name
+  base: '/', // Serve site at the root of GitHub Pages
   server: {
     port: 5173,
   },
@@ -22,4 +22,4 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
   },
-}); 
+});
