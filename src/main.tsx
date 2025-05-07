c865de57383329373a8dec7d81ad7f18550de4eb
Franco Nogarin <spydmobile@gmail.com>
2025-05-07 06:42:41 -0600

    Remove StoreNavBar and CartContext components from the project

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './style.css';
-import { CartProvider } from './context/CartContext';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter basename="/">
-      <CartProvider>
+      
         <App />
-      </CartProvider>
+      
     </BrowserRouter>
   </React.StrictMode>
 );
