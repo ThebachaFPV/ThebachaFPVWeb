03011e4c4bca9b9ad21028dc2713cf711669ce41
Franco Nogarin <spydmobile@gmail.com>
2025-05-07 08:20:18 -0600

    Replace anchor tag with Link component in Footer for improved routing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer: React.FC = () => {
   return (
     <footer >
-      <p>&copy; 2025 Thebacha FPV Racing League. All rights reserved. <span style={{fontSize: 'smaller'}}>| <a href="/shop" style={{color: 'inherit', textDecoration: 'underline'}}>Shop</a></span></p>
+      <p>&copy; 2025 Thebacha FPV Racing League. All rights reserved. <span style={{fontSize: 'smaller'}}>| <Link to="/shop" style={{color: 'inherit', textDecoration: 'underline'}}>Shop</Link></span></p>
       <div style={{fontSize: '0.9rem', marginTop: '0.5rem', color: 'var(--white)'}}>Last modified: May 7, 2025</div>
     </footer>
   );
