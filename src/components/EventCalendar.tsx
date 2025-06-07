a27691e8915e4d23b487389d782151571ce66d7c
Franco Nogarin <spydmobile@gmail.com>
2025-06-07 17:22:34 -0600

    Add missing training event on May 25, 2025, to EventCalendar

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -44,13 +44,13 @@ const staticEvents: Event[] = [
   //   end: new Date('2025-05-11T17:00:00'),
   //   description: "May 11, 2025: Training/Practice at The Fort Smith Recreation Center Gym, 3:30pm–5pm"
   // },
-  // {
-  //   title: "Training/Practice",
-  //   location: "The Fort Smith Recreation Center Gym",
-  //   start: new Date('2025-05-25T15:30:00'),
-  //   end: new Date('2025-05-25T17:00:00'),
-  //   description: "May 25, 2025: Training/Practice at The Fort Smith Recreation Center Gym, 3:30pm–5pm"
-  // },
+  {
+    title: "Training/Practice",
+    location: "The Fort Smith Recreation Center Gym",
+    start: new Date('2025-05-25T15:30:00'),
+    end: new Date('2025-05-25T17:00:00'),
+    description: "May 25, 2025: Training/Practice at The Fort Smith Recreation Center Gym, 3:30pm–5pm"
+  },
   {
     title: "Training/Practice",
     location: "The Fort Smith Recreation Center Gym",
@@ -65,7 +65,7 @@ const staticEvents: Event[] = [
     end: new Date('2025-06-22T17:00:00'),
     description: "June 22, 2025: Training/Practice at The Fort Smith Recreation Center Gym, 3:30pm–5pm"
   },
-  {
+   {
     title: "Training/Practice",
     location: "The Fort Smith Recreation Center Gym",
     start: new Date('2025-07-06T15:30:00'),
