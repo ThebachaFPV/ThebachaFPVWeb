6ff065fe7561f22bba8de4fa19d4c9ec182910eb
Franco Nogarin <spydmobile@gmail.com>
2025-10-26 12:57:09 -0600

    Update last modified date in footer to Oct 26, 2025

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer >
       <p>&copy; 2025 Thebacha FPV Racing League. All rights reserved. <span style={{ fontSize: 'smaller' }}>| <Link to="/shop" style={{ color: 'inherit', textDecoration: 'underline' }}>Shop</Link></span></p>
-      <div style={{ fontSize: '0.9rem', marginTop: '0.5rem', color: 'var(--white)' }}>Last modified: May 15, 2025</div>
+      <div style={{ fontSize: '0.9rem', marginTop: '0.5rem', color: 'var(--white)' }}>Last modified: Oct 26, 2025</div>
       <p>Our organization is grateful for ongoing support from </p>
       <a href='https://www.nwtrpa.org&referred-by=thebachafpv.org' target='_blank'><img src="/assets/sponsors/NWTRPA-Logo.png" alt="Parks and Recreation Logo" style={{ maxWidth: '300px', height: 'auto' }} /></a>
       <br />
